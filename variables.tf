a4c28163fd45742078053f0f51d44b2672dd5513
Volodymyr Shutov <160865233+v-shutov@users.noreply.github.com>
2024-09-23 13:05:31 +0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default     = "example-resources"
+}
+
+variable "location" {
+  description = "The location for all resources."
+  default     = "West Europe"
+}
