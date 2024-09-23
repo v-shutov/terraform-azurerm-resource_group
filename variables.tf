eb0a67a3bb185e9feec3a1b5ab590c7aee76ad02
Volodymyr Shutov <forshutov@gmail.com>
2024-09-23 15:07:44 +0400

    fmt

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
-  type = string
+  type        = string
 }
 
 variable "location" {
   description = "The location for all resources."
-  type = string
+  type        = string
 }
 
 variable "storage_account_name" {
   description = "The name of the storage account"
-  type = string
+  type        = string
 }
